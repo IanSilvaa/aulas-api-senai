fefa42a9df1d848aceb084355345e821ae192f6d
IAN FERREIRA DA SILVA <ian_f_silva@estudante.sesisenai.org.br>
2024-05-16 21:55:08 -0300

    continuação da api de usuarios

--- src/index.js
+++ src/index.js
@@ -4,27 +4,59 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 
-app.get('/usuarios', (req, res) => {
-    console.log(req.query);
-    res.send('Chamou o GET!');
-});
-app.post('/usuarios', (req, res) => {
-    console.log(req.body);
-    res.send(`Chamou o POST com o nome: ${req.body.nome}`);
-});
-app.put('/usuarios', (req, res) => {
-    console.log(req.headers);
+// Variável em memória para simular usuários cadastrados no banco de dados
+let usuarios = []
 
-    if (!req.headers.autorizacao){
-        res.status(401).send('Informe o HEADER "autorizacao"')
-    }else{
-        res.send('Chamou o PUT!');
+app.get('/usuarios', (req, res) => {
+    let resultado = usuarios;
+    // faz o filtro se receber ?filtro=exemplo
+    if (req.query.filtro) {
+        resultado = usuarios.filter((u) => {
+            return u.nome.includes(req.query.filtro);
+        });
     }
 
+    res.send(resultado);
 });
-app.delete('/usuarios/:id', (req, res) => {
-    console.log(req.params);
-    res.send(`Chamou o DELETE com id: ${req.params.id}`);
+
+app.post('/usuarios', (req, res) => {
+    console.log(req.body);
+    if (!req.body || !req.body.nome || !req.body.email) {
+        res.status(400).send('Os campos nome e email são obrigatórios!');
+        return;
+    }
+
+    const usuarioJaExiste = usuarios.find(usu => usu.email === req.body.email);
+    if (usuarioJaExiste) {
+        res.status(409).send('Usuário já cadastrado');
+        return;
+    }
+
+    // Vamos gerar um id aleatório apenas de exemplo
+    const novoUsuario = { ...req.body, id: +new Date() };
+
+    usuarios.push(novoUsuario)
+    res.set(201).send(novoUsuario);
+});
+
+app.put('/usuarios', (req, res) => {
+    usuarios = usuarios.map((xxx) => {
+        if (xxx.id === req.body.id) {
+            return req.body
+        } else {
+            return xxx;
+        }
+    });
+
+    res.send('Operação efetuada com sucesso');
+});
+
+app.delete('/usuarios/:idUsuario', (req, res) => {
+    usuarios = usuarios.filter((xxx) => {
+        return xxx.id !== +req.params.idUsuario;
+    })
+
+    res.send('Operação efetuada com sucesso!');
 });
 
 app.listen(port, () => {
